1b63f29628c601490aaef1df2b6b516143102d6e
guih0113 <gui.henriquess13@gmail.com>
2025-07-23 00:48:23 -0300

    add hook form and zod validations

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,49 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 import { FaGithub, FaGoogle, FaTwitter } from 'react-icons/fa'
+import z from 'zod'
+
+const registerFormSchema = z.object({
+  email: z.email(),
+  username: z.string().min(5, 'O nome de usuário deve ter, no mínimo, 5 caracteres.'),
+  password: z
+    .string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/,
+      'A senha deve conter pelo menos uma letra minúscula, uma maiúscula, um número, um caractere especial e ter no mínimo 8 caracteres.',
+    ),
+})
+
+type RegisterFormSchema = z.infer<typeof registerFormSchema>
 
 export default function Register() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormSchema>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      email: '',
+      username: '',
+      password: '',
+    },
+  })
+
+  function handleCreateUser() {
+    console.log('test')
+    reset()
+  }
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div
-        className="flex w-90 flex-col items-center justify-center gap-8 overflow-hidden rounded-[20px] bg-black/80 py-7"
+        className="flex w-md flex-col items-center justify-center gap-8 overflow-hidden rounded-[20px] bg-black/80 py-7"
         style={{ boxShadow: '0 0 30px rgba(255, 255, 255, 0.15)' }}
       >
         <div className="mt-2">
@@ -18,35 +55,56 @@ export default function Register() {
           <p className="text-sm text-zinc-400">Seja vem vindo de volta, sentimos sua falta!</p>
         </div>
 
-        <form className="w-full space-y-5 px-10">
+        <form onSubmit={handleSubmit(handleCreateUser)} className="w-full space-y-5 px-10">
           <div>
             <label htmlFor="email" className="text-sm">
               E-mail Address
             </label>
-            <div className="relative mt-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
+            <div className="relative mt-2 mb-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
               <span className="absolute">📧</span>
-              <input type="email" placeholder="yourname@gmail.com" id="email" className="ml-8 outline-none" />
+              <input
+                type="email"
+                {...register('email')}
+                placeholder="yourname@gmail.com"
+                id="email"
+                className="ml-8 outline-none"
+              />
             </div>
+            {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
           </div>
 
           <div>
             <label htmlFor="username" className="text-sm">
               Username
             </label>
-            <div className="relative mt-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
+            <div className="relative mt-2 mb-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
               <span className="absolute">👤</span>
-              <input type="email" placeholder="@username" id="username" className="ml-8 outline-none" />
+              <input
+                type="text"
+                {...register('username')}
+                placeholder="@username"
+                id="username"
+                className="ml-8 outline-none"
+              />
             </div>
+            {errors.username && <span className="text-red-500 text-sm">{errors.username.message}</span>}
           </div>
 
           <div>
             <label htmlFor="password" className="text-sm">
               Password
             </label>
-            <div className="relative mt-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
+            <div className="relative mt-2 mb-2 rounded-md bg-zinc-700 px-3 py-3 text-sm">
               <span className="absolute">🔒</span>
-              <input type="password" placeholder="Password" id="password" className="ml-8 outline-none" />
+              <input
+                type="password"
+                {...register('password')}
+                placeholder="Password"
+                id="password"
+                className="ml-8 outline-none"
+              />
             </div>
+            {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
           </div>
 
           <input
